b0adbb5227f1d7a381ec53a6c9078cefa31cb0c0
Jafarbek Yusupov <j.yusupov@newuu.uz>
2024-10-28 13:40:31 +0500

    Update problems.h

--- problems.h
+++ problems.h
@@ -1,47 +1,39 @@
-// Problem 1
-template <typename T>
-int floorFunction(T n) {
+template <typename T> int floorFunction(T n) {
     int intp = static_cast<int>(n);
     if (n > 0 && n != intp){    return intp - 1;}
     return intp;
 }
 
-template <typename T>
-int ceilingFunction(T n) {
+template <typename T> int ceilingFunction(T n) {
     int intp = static_cast<int>(n);
     if (n > 0 && n != intp){    return intp + 1;}
     return intp;
 }
 
-// Problem 2
-template <typename T>
-void swap_values(T& a, T& b) {
+template <typename T> void swap_values(T& a, T& b) {
     a += b;
     b = a - b;
     a -= b;
 }
-// Problem 3
+
 int multiply(int x, int y) {
     if (y == 0){    return 0;}
     if (y < 0) {    return -multiply(x, -y);}
     return x + multiply(x, y - 1);
 }
 
-// Problem 4
 int digitSum(int n) {
     if (n == 0){    return 0;}
     return (n % 10) + digitSum(n / 10);
 }
-// Problem 5
+
 void decimalToBinary(int n) {
     if (n == 0) {    return;}
     decimalToBinary(n / 2);
     std::cout << (n % 2);
 }
 
-// Problem 6
-template <typename T>
-T midValue(T a, T b, T c) {
+template <typename T> T midValue(T a, T b, T c) {
     if ((a > b && a < c) || (a < b && a > c)) {
         return a;
     } else if ((b > a && b < c) || (b < a && b > c)) {
